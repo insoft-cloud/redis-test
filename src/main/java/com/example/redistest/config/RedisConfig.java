d1d81c6993c3e557ddc120f005fe750fa6790de9
smchoi98 <smchoi@in-soft.co.kr>
2023-04-10 17:03:44 +0900

    init

--- src/main/java/com/example/redistest/config/RedisConfig.java
+++ src/main/java/com/example/redistest/config/RedisConfig.java
@@ -1,21 +1,18 @@
 package com.example.redistest.config;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
 @EnableRedisHttpSession
 public class RedisConfig {
-	@Autowired
-	RedisClusterConfigurationProperties clusterProperties;
+	// @Autowired
+	// RedisClusterConfigurationProperties clusterProperties;
 
 	@Value("${spring.data.redis.password}")
 	private String password;
@@ -44,20 +41,7 @@ public class RedisConfig {
 	public RedisTemplate<String, Object> redisTemplate() {
 		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
 		redisTemplate.setConnectionFactory(redisConnectionFactory());
-		redisTemplate.setKeySerializer(new StringRedisSerializer());
-		redisTemplate.setValueSerializer(new StringRedisSerializer());
+		redisTemplate.setDefaultSerializer(new StringRedisSerializer());
 		return redisTemplate;
-	}
-
-	@Bean
-	public StringRedisTemplate stringRedisTemplate() {
-		StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
-		stringRedisTemplate.setKeySerializer(new StringRedisSerializer());
-		stringRedisTemplate.setValueSerializer(new StringRedisSerializer());
-		stringRedisTemplate.setDefaultSerializer(new StringRedisSerializer());
-		stringRedisTemplate.setConnectionFactory(redisConnectionFactory());
-		stringRedisTemplate.afterPropertiesSet();
-
-		return stringRedisTemplate;
 	}
 }
