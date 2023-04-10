48f5dbe12f94e8c877855bc6ce92c3d15e5424e7
smchoi98 <smchoi@in-soft.co.kr>
2023-04-10 16:33:52 +0900

    Initial commit

---
+++ src/main/java/com/example/redistest/config/RedisClusterConfigurationProperties.java
@@ -0,0 +1,9 @@
+package com.example.redistest.config;/**
+ * @package : com.example.redistest.config
+ * @Project : redis-test
+ * @FileName : RedisClusterConfigurationProperties
+ * @Date : 2023-04-10
+ * @author : INSoft
+ * @description :
+ */public class RedisClusterConfigurationProperties {
+}
